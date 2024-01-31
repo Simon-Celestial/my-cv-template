1e978332a5081f3ebcfe4d3b7c819d1e1927a92a
Gasimli Ziya <gasimli.ziya@yandex.com>
2024-01-31 19:58:30 +0400

    image animation done

--- src/Layout/Home/Home.jsx
+++ src/Layout/Home/Home.jsx
@@ -5,6 +5,21 @@ export const Home = () => {
     return (
         <div className={styles.pageWrapper}>
             <div className={styles.pageContent}>
+                <div className={styles.myName}>
+                    <div className={styles.logoBox}>
+                        <p>Z</p>
+                    </div>
+                    <span>Gasimli <p>Ziya</p></span>
+                </div>
+                <div className={styles.infoContainer}>
+                    <div className={styles.imageBox}>
+                        <img src="../../../public/Assets/Images/myImg.jpg" alt="Me"/>
+                        <div className={styles.rotateBlock}></div>
+                    </div>
+                    <div className={styles.infoContent}>
+
+                    </div>
+                </div>
 
             </div>
 
