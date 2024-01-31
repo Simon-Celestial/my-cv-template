58d5e4f3c9d71ca812e23a19f727bea9c9b652ba
Gasimli Ziya <gasimli.ziya@yandex.com>
2024-01-31 18:00:19 +0400

    app started

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Home} from "./Layout/Home/Home.jsx";
+
+
+export const App = () => {
+
+    return (
+        <Home />
+    )
+}
---
+++ src/Layout/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./Home.module.scss";
+
+export const Home = () => {
+    return (
+        <div className={styles.pageWrapper}>
+            <div className={styles.pageContent}>
+
+            </div>
+
+        </div>
+    )
+}
