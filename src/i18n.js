27e6c5bd1c3fea948b3dd0ba2deebd9baf392614
Gasimli Ziya <gasimli.ziya@yandex.com>
2024-04-26 03:24:45 +0400

    new fix

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,9 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translationTR from "../public/translations/tr.json";
-import translationEN from "../public/translations/en.json";
-import translationRU from "../public/translations/ru.json";
-import translationAZ from "../public/translations/az.json";
+import translationTR from "/public/translations/tr.json";
+import translationEN from "/public/translations/en.json";
+import translationRU from "/public/translations/ru.json";
+import translationAZ from "/public/translations/az.json";
 
 i18n
     .use(initReactI18next)
