7f49b612819b9da446c2b0eb2424df36c3b8c42c
Gasimli Ziya <gasimli.ziya@yandex.com>
2024-04-26 02:59:11 +0400

    new fix

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '/public/translations/tr.json',
-        '/public/translations/en.json',
-        '/public/translations/ru.json',
-        '/public/translations/az.json'
-      ]
-    }
-  }
 })
 
